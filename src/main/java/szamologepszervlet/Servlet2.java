bdbf248d24b6166194b4d24e9374489588cbf301
tothd <tothd@ASUS-ROG--David.mshome.net>
2021-09-28 09:11:23 +0200

    Óra végi állapot.

--- src/main/java/szamologepszervlet/Servlet2.java
+++ src/main/java/szamologepszervlet/Servlet2.java
@@ -1,6 +1,8 @@
 package szamologepszervlet;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
@@ -12,19 +14,65 @@ import jakarta.servlet.http.HttpServletResponse;
 @WebServlet("/szamologep.do")
 public class Servlet2 extends HttpServlet{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		ResultDto resultDto = new ResultDto();
+		List<String> errorList = new ArrayList<String>(); 
+		
 		String aString = req.getParameter("a");
+		Double a = 0.0;
+		if (aString == null) {
+				errorList.add("az 'a' parameter nem lehet ures");
+		} else {
+			try {
+				a = Double.parseDouble(aString);
+			} catch (NumberFormatException ex) {
+				errorList.add("az 'a' parameter szam kell legyen");
+			}
+		}
+		resultDto.setA(aString);
+		
+		
 		String bString = req.getParameter("b");
+		Double b = 0.0;
+		if (bString == null) {
+				errorList.add("az 'b' parameter nem lehet ures");
+		} else {
+			try {
+				b = Double.parseDouble(bString);
+			} catch (NumberFormatException ex) {
+				errorList.add("az 'b' parameter szam kell legyen");
+			}
+		}
+		resultDto.setB(bString);
+		
+		
 		String operator = req.getParameter("operator");
-
-		Double result = 0.0;
-		if ("+".equals(operator)) {
-			result = Double.parseDouble(aString) + Double.parseDouble(bString);
+		resultDto.setOperator(operator);
+		
+		if (operator == null) {
+			errorList.add("az operator parameter nem lehet ures");
 		}
 		
-		// eltarolja a keresbe, hogy a jsp is elerje
-		req.setAttribute("result", result);
+		
+		Double result = 0.0;
+		if (errorList.isEmpty()) {
+			if ("+".equals(operator)) {
+				result = a + b;
+			} else {
+				errorList.add("nem tamogatott muvelet");	
+			}
+		}
+		
+		resultDto.setResult(result);
+		resultDto.setErrorList(errorList);
+		// eltarolja a keres attributumba az osszes infot, hogy a jsp is elerje
+		req.setAttribute("result", resultDto);
 		
 		// forward
 		RequestDispatcher rd = req.getSession()
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -4,14 +4,20 @@
 </head>
 <body>
 <%
-Double result = (Double) request.getAttribute("result");
+//ha a servlettol jott, akkor van, ha elso keres, akkor nincs
+szamologepszervlet.ResultDto result = (request.getAttribute("result") != null) ?
+					(szamologepszervlet.ResultDto) request.getAttribute("result") :
+					new szamologepszervlet.ResultDto();
 
 String resultText = (result == null) ?
-				"" : "result " + result.toString();
+				"" : "result " + result.getResult().toString();
 %>
-<form method="post" action="szamologep.do">
-    <input type="text" name="a" /><br>
-    <input type="text" name="b" /><br>
+
+ver: 0.4<br>
+
+<form method="post" action="/szamologepszervlet/szamologep.do">
+    <input type="text" name="a" value="<%= result.getA() %>"/><br>
+    <input type="text" name="b" value="<%= result.getB() %>"/><br>
     <select name="operator">
         <option value="+" /> +
         <option value="-" /> -
